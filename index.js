bf154c982bbc78f0bb95359e12a84817f9b7edab
zrichardet <zach@tetriscodes.com>
2019-08-07 13:59:57 -0500

    Adding circle ci

--- index.js
+++ index.js
@@ -1,13 +1,91 @@
-//noIncome expects two floats for income and expense
-function noIncome(income, expense) {
-  return income - expense;
+export function sumExpenses(
+  monthlyMortgage,
+  monthlyIncome,
+  vacancyRate,
+  managementFee,
+  maintenanceFee,
+  capitalExpenditures
+) {
+  let totalExpenses =
+    monthlyMortgage * 1 +
+    monthlyIncome * vacancyRate +
+    monthlyIncome * managementFee +
+    monthlyIncome * maintenanceFee +
+    monthlyIncome * capitalExpenditures;
+  return totalExpenses;
 }
 
-function roiPercent(noi, investmentAmount) {
-  return (noi / investmentAmount) * 100;
+export function calculateNOI(monthlyIncome, totalExpenses) {
+  return monthlyIncome * 12 - totalExpenses * 12;
+}
+export function calculateROI(
+  monthlyIncome,
+  purchasePrice,
+  downPayment,
+  totalExpenses
+) {
+  let ROI =
+    (calculateNOI(monthlyIncome, totalExpenses) /
+      (purchasePrice * downPayment)) *
+    100;
+  return ROI.toFixed(1);
+}
+export function makeReturnTable(years, monthlyIncome, totalExpenses) {
+  let table = [];
+  [...Array(years).keys()].forEach(i => {
+    let returnObject = {
+      year: i,
+      return: calculateNOI(monthlyIncome, totalExpenses) * (i + 1)
+    };
+    table.push(returnObject);
+  });
+
+  return table;
 }
 
-module.exports = {
-  noIncome: noIncome,
-  roiPercent: roiPercent
-};
+export function mortgagePayment(interestRate, loanTerm, loanAmount) {
+  let monthlyRate = interestRate / 12;
+  let numOfPayments = loanTerm * 12;
+  let numerator = monthlyRate * (1 + monthlyRate) ** numOfPayments;
+  let denominator = (1 + monthlyRate) ** numOfPayments - 1;
+  let monthlyMortgage = loanAmount * (numerator / denominator);
+  return monthlyMortgage.toFixed(2);
+}
+
+export function interestPayment(loanAmount, interestRate) {
+  let interestPayment = loanAmount * (interestRate / 12);
+  return interestPayment.toFixed(2);
+}
+
+export function principalPayment(loanAmount, loanTerm, interestRate) {
+  let principalPayment =
+    mortgagePayment(interestRate, loanTerm, loanAmount) -
+    interestPayment(loanAmount, interestRate);
+  return principalPayment;
+}
+
+export function loanPaydown(loanAmount, loanTerm, interestRate) {
+  let loanBalance =
+    loanAmount - principalPayment(loanAmount, loanTerm, interestRate);
+  return loanBalance;
+}
+
+export function makeBalanceTable(
+  loanTermYears,
+  loanAmount /*int*/,
+  interestRate /*float*/
+) {
+  let table = [];
+  let newLoanAmount = loanAmount;
+  [...Array(loanTermYears).keys()].forEach(i => {
+    newLoanAmount =
+      newLoanAmount - loanPaydown(newLoanAmount, loanTermYears, interestRate);
+    let returnObject = {
+      year: i,
+      return: newLoanAmount
+    };
+    table.push(returnObject);
+  });
+
+  return table;
+}
--- index.test.js
+++ index.test.js
@@ -1,27 +1,38 @@
 var expect = require("chai").expect;
 var calculators = require("./index");
 
-describe("calculators", function() {
-  describe("noIncome", function() {
-    it("should be a float", function() {
-      expect(calculators.noIncome(8, 0.5)).to.be.closeTo(7.5, 0.0000000001);
-    });
-    it("test no income", function() {
-      expect(calculators.noIncome(0.0, 0.5)).to.be.closeTo(-0.5, 0.0000000001);
-    });
-    it("test no expenses", function() {
-      expect(calculators.noIncome(9, 0.0)).to.be.closeTo(9.0, 0.0000000001);
-    });
-  });
+// describe("calculators", function() {
+//   describe("noIncome", function() {
+//     it("should be a float", function() {
+//       expect(calculators.noIncome(8, 0.5)).to.be.closeTo(7.5, 0.0000000001);
+//     });
+//     it("test no income", function() {
+//       expect(calculators.noIncome(0.0, 0.5)).to.be.closeTo(-0.5, 0.0000000001);
+//     });
+//     it("test no expenses", function() {
+//       expect(calculators.noIncome(9, 0.0)).to.be.closeTo(9.0, 0.0000000001);
+//     });
+//   });
 
-  describe("calculators", function() {
-    describe("roiPercent", function() {
-      it("Should be a value less than 100", function() {
-        expect(calculators.roiPercent(1, 1)).to.be.closeTo(100, 0.000000001);
-      });
-      it("Should be greater than -100", function() {
-        expect(calculators.roiPercent(-1, 1)).to.be.closeTo(-100, 0.0000001);
-      });
+//   describe("calculators", function() {
+//     describe("roiPercent", function() {
+//       it("Should be a value less than 100", function() {
+//         expect(calculators.roiPercent(1, 1)).to.be.closeTo(100, 0.000000001);
+//       });
+//       it("Should be greater than -100", function() {
+//         expect(calculators.roiPercent(-1, 1)).to.be.closeTo(-100, 0.0000001);
+//       });
+//     });
+//   });
+// });
+
+describe("Expenses", () => {
+  describe("Sum", () => {
+    it("Should sum expenses to 10", () => {
+      expect(calculators.sumExpenses(1, 1, 0.1, 0.1, 0.1, 0.1)).to.be.closeTo(
+        1.4,
+        0.00001
+      );
     });
   });
 });
